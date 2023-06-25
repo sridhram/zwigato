9173eabbd86aa4ab8b3b3fc2a720e956aa2ce0db
sridhram <srdhrramkumar@gmail.com>
2023-06-25 15:09:00 +0530

    redux store setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import {AnimatePresence} from 'framer-motion'
+import {createStore} from "redux";
+import { Provider } from 'react-redux';
+import combinedReducers from './context/reducers/index.js'
+
+
+const myStore = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AnimatePresence>
-        <App />
+        <Provider store={myStore}>
+          <App />
+        </Provider>
       </AnimatePresence>
     </BrowserRouter>
   </React.StrictMode>,
